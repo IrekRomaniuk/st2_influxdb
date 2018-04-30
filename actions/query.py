592a24a415e6ee06d5bb5b2c8f31d7efde813fc6
irom77 <aniairom0>
2018-04-30 14:24:11 -0400

    init

--- actions/query.py
+++ actions/query.py
@@ -18,7 +18,7 @@ class query(Action):
         _pass = self.config['password']
         _base_url, _port = self.config['base_url'].split(":") 
         print(_db,_user,_pass,_base_url)    
-        client = InfluxDBClient(_base_url, 8086, _user', _pass, _db)
+        client = InfluxDBClient(_base_url, 8086, _user, _pass, _db)
         query="select {0} from {1} WHERE time > now() - {1}s;".format(value, measurement, WINDOW)
         result = client.query(query)
         points = list(result.get_points(measurement=measurement, tags=tags))
