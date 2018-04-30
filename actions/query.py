802dece2213dee720ca9bed58a8dff549a334427
Irek Romaniuk <IrekRomaniuk@Commonwealth.com>
2018-04-30 08:19:48 -0400

    initial

--- actions/query.py
+++ actions/query.py
@@ -12,13 +12,13 @@ WINDOW=600
 result_list=[]
 class query(Action):
        
-    def run(self, measurement, tags):
+    def run(self, , value, measurement, tags):
         _db = self.config['db']
         _user = self.config['username']
         _pass = self.config['password']
         _base_url, _port = self.config['base_url'].split(":") 
         client = InfluxDBClient(_base_url, 8086, _user', _pass, _db)
-        query="select {0} from cpu WHERE time > now() - {1}s;".format(measurement, WINDOW)
+        query="select {0} from {1} WHERE time > now() - {1}s;".format(value, measurement, WINDOW)
         result = client.query(query)
         points = list(result.get_points(measurement=measurement, tags=tags))
         for point in points:
