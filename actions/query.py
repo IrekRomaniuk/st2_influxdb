5d3f2067579131f19cc1c9dc5675d86f77db6645
Irek Romaniuk <IrekRomaniuk@Commonwealth.com>
2018-04-30 08:11:12 -0400

    initial

--- actions/query.py
+++ actions/query.py
@@ -1,23 +1,29 @@
 import sys, requests, xmltodict, json, urllib3
 from string import Template
+from influxdb import InfluxDBClient
+import json
 
 from st2actions.runners.pythonrunner import Action
 """
-curl -kG ‘https://10.110.111.10:8086/query?pretty=true' — data-urlencode “db=NOAA_water_database” — data-urlencode “q=SELECT * FROM h2o_feet LIMIT 3” -u “phantom:phantom” | jq ‘.’ 
+python test/influx.py
 """
+CPUMAX=20
+WINDOW=600
+result_list=[]
 class query(Action):
        
-    def run(self, db, user, pass):
+    def run(self, measurement, tags):
         _db = self.config['db']
         _user = self.config['username']
         _pass = self.config['password']
-        _base_url = self.config['base_url']
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        url = "https://" + _base_url + "/query?pretty=true"
-        try:
-            response = requests.post()
-        except requests.exceptions.ConnectionError:
-            _result['"{}"'.format(firewall)] = "ConnectionError"
-            return (False, _result)
-
-        return (True, _result)    
+        _base_url, _port = self.config['base_url'].split(":") 
+        client = InfluxDBClient(_base_url, 8086, _user', _pass, _db)
+        query="select {0} from cpu WHERE time > now() - {1}s;".format(measurement, WINDOW)
+        result = client.query(query)
+        points = list(result.get_points(measurement=measurement, tags=tags))
+        for point in points:
+            string_point=dict([(str(k), str(v)) for k, v in point.items()])
+            if int(string_point['value'])>CPUMAX:
+            #print(string_point)
+            result_list.append(string_point)
+        return (string_point)    
