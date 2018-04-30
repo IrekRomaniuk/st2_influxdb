909d0b7335cc4c67a080ce4b9467622c053625ee
irom77 <aniairom0>
2018-04-30 14:47:33 -0400

    init

--- actions/query.py
+++ actions/query.py
@@ -4,17 +4,17 @@ from st2actions.runners.pythonrunner import Action
 """
 python test/influx.py
 """
-
+CPUMAX=20
+WINDOW=600
+result_list=[]
 class query(Action):
-    CPUMAX=20
-    WINDOW=600
-    result_list=[]   
-    def run(self, value, measurement, tags):
+       
+    def run(self, value="*", measurement, tags={}):
         _db = self.config['db']
         _user = self.config['username']
         _pass = self.config['password']
         _base_url, _port = self.config['base_url'].split(":") 
-        print(_db,_user,_pass,_base_url, _port)    
+        print(_db,_user, _pass, _base_url, _port)    
         client = InfluxDBClient(_base_url, _port, _user, _pass, _db)
         query="select {0} from {1} WHERE time > now() - {2}s;".format(value, measurement, WINDOW)
         result = client.query(query)
