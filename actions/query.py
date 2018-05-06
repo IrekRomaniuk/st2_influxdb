b2cd0445dc45d4cc3744d1d5fc7fdbd486e43e42
Irek Romaniuk <IrekRomaniuk@Commonwealth.com>
2018-05-06 16:57:14 -0400

    remove tags

--- actions/query.py
+++ actions/query.py
@@ -9,7 +9,7 @@ WINDOW=60
 result_list=[]
 class query(Action):
        
-    def run(self, value, measurement, tags):
+    def run(self, value, measurement): #, tags
         _db = self.config['db']
         _user = self.config['username']
         _pass = self.config['password']
@@ -18,7 +18,7 @@ class query(Action):
         client = InfluxDBClient(_base_url, _port, _user, _pass, _db)
         query="select {0} from {1} WHERE time > now() - {2}s;".format(value, measurement, WINDOW)
         result = client.query(query)
-        points = list(result.get_points(measurement=measurement, tags=tags))
+        points = list(result.get_points(measurement=measurement)) #, tags=tags
         for point in points:
             string_point=dict([(str(k), str(v)) for k, v in point.items()])
             if int(string_point['value'])>CPUMAX:
