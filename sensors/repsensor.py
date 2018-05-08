ae4d0f613a7048a13d4e4a78ae1f338d033a76cf
Irek Romaniuk <IrekRomaniuk@Commonwealth.com>
2018-05-08 15:25:17 -0400

    changeing to minimum

--- sensors/repsensor.py
+++ sensors/repsensor.py
@@ -38,23 +38,34 @@ class RepvpnSensor(PollingSensor):
         # count = self.sensor_service.get_value('influxdb.count') or 0
         result = self._client.query(self._query)
         points = list(result.get_points(measurement=MEASUREMENT)) #, tags=tags
-        max = {}
-        max_all = 0
+        minimum = {}
+        # max_all = 0
         alert_saved = self.sensor_service.get_value('influxdb.alert') or False
+        alert = False
         payload = {}
         for point in points:
             string_point=dict([(str(k), str(v)) for k, v in point.items()])
             i = string_point['site'] + ":" + string_point['firewall'] + ":" + string_point['id'] + ":" + string_point['proc']
-            if i not in max:
-                max[i] = 0
-            if int(string_point['value']) > max[i]:
+            if i not in minimum:
+                minimum[i] = 100
+            if int(string_point['value']) < minimum[i]:
                 # print(string_point)               
-                max[i] = int(string_point['value'])
-                payload[i]=int(max[i])
-                #self.sensor_service.set_value('influxdb.alert', True)
-                if int(string_point['value']) > max_all:
-                    max_all = int(string_point['value'])
-                    payload['max_all'] = int(string_point['value'])                    
+                minimum[i] = int(string_point['value'])
+                payload[i]=int(minimum[i])                
+                # if int(string_point['value']) > max_all:
+                    # max_all = int(string_point['value'])
+                    # payload['max_all'] = int(string_point['value'])                    
+        for i, cpu in minimum.iteritems():
+            if cpu > self._max:
+                alert = True
+                if alert_save != alert:
+                    payload['alert'] = True
+                    self.sensor_service.set_value('influxdb.alert', True)
+                    payload['minimum'] = cpu
+                    payload['alerted'] = i
+                else:
+                    payload['alert'] = False
+                    self.sensor_service.set_value('influxdb.alert', False)    
 
         payload['num_pts'] = len(points)
         payload['max'] = int(self.sensor_service.get_value('influxdb.max')) or 98
