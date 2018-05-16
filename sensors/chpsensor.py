28ef5bae401b9550a6958c42c00c81138c19e0b0
Irek Romaniuk <IrekRomaniuk@Commonwealth.com>
2018-05-16 12:46:53 -0400

    fixing

--- sensors/chpsensor.py
+++ sensors/chpsensor.py
@@ -7,7 +7,7 @@ class ChpSensor(RepvpnSensor):
     """
     def __init__(self, sensor_service, config, poll_interval):             
         super(ChpSensor, self).__init__(sensor_service=sensor_service, config=cpnfig, poll_interval=poll_interval, 
-                                        value='value', measurement='cpu', value='value', measurement='cpu', 
+                                        value='value', measurement='cpu', 
                                         tags =['site','firewall', 'id', 'proc'], skip_zero=True)
         # RepvpnSensor.__init__(self, value = 'value', measurement = 'cpu', tags = ['site','firewall', 'id', 'proc'], skip_zero = True)
     
