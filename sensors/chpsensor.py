08e0c2c12bf08168a3cd8e4a3aa4319737a52078
Irek Romaniuk <IrekRomaniuk@Commonwealth.com>
2018-05-16 11:52:21 -0400

    fixing

--- sensors/chpsensor.py
+++ sensors/chpsensor.py
@@ -6,6 +6,6 @@ class ChpSensor(RepvpnSensor):
     curl -i -XPOST 'http://1.1.1.1:8086/write?db=firewalls' --data-binary 'cpu,firewall=TST,site=TST value=90'    
     """
     def __init__(self):             
-        super(ChpSensor, self).__init__(sensor_service=sensor_service, config=config, poll_interval=poll_interval,
+        super(ChpSensor, self).__init__(sensor_service=sensor_service, config=None, poll_interval=None,
                                         value = 'value', measurement = 'cpu', tags = ['site','firewall', 'id', 'proc'], skip_zero = True)
     
