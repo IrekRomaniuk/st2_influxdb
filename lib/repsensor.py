35aa5edffccc31bd7c7b124c118b88b974abd461
Irek Romaniuk <IrekRomaniuk@Commonwealth.com>
2018-05-16 11:58:26 -0400

    fixing

--- lib/repsensor.py
+++ lib/repsensor.py
@@ -21,8 +21,7 @@ class RepvpnSensor(PollingSensor):
     curl -i -XPOST 'http://1.1.1.1:8086/write?db=firewalls' --data-binary 'cpu,firewall=TST,site=TST value=90'    
     """
     def __init__(self, sensor_service, poll_interval, value, measurement, tags, skip_zero): # sensor_service, config=None, poll_interval=None, 
-        super(RepvpnSensor, self).__init__(sensor_service=sensor_service, 
-                                          config=config,
+        super(RepvpnSensor, self).__init__(sensor_service=sensor_service,                                        
                                           poll_interval=poll_interval)
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
           
